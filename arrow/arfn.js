fd5045bebaf47eee5c39f2483f47e792ca8bd857
Prabhat <prabhat22pk@gmail.com>
2025-06-30 16:05:52 +0530

    new files

--- arrow/arfn.js
+++ arrow/arfn.js
@@ -1,14 +1,22 @@
-const user = {
-    username: "prabhat",
-    price: "999",
+// const user = {
+//     username: "prabhat",
+//     price: "999",
     
-    welcomeMessage: function(){
-        console.log(`${this.username} , welcome to the website`);
-        console.log(this);
-        
-    }
-}
-user.welcomeMessage() 
+//     welcomeMessage: function(){
+//         console.log(`${this.username} , welcome to the website`);        
+//     }
+// }
+// user.welcomeMessage() 
 
 //functions k andar this ka context undefined hota hai and it works inside the object 
 
+//implicit return
+
+// const add = (num1 ,num2) => (num1+num2) //ye wala format se hum object return kar skte hai like  ( { } ) this is called as implicit return
+// console.log(add(1,2))
+
+//immediately invoked functions
+
+(function chai(){
+    console.log("hello")
+}) ()  //one bracket pair is for definition and other is for invoking khudse chlgya //we can also pass arguments as we pass is normal functions
---
+++ execution/index.js
@@ -0,0 +1,11 @@
+//*********************************************Execution context ****************************************************/
+
+//excetion context is stored in this keyword and it is different for different environments like for node it returns {}
+//for browser environments it is the window object
+//how javascript code is executed? :: 
+// 1. Memory creation phase
+// : saare variables ko lia jata h or unke andr undefined daaldia jata hai , isko bolte hai first cycle
+
+
+// 2. Execution phase 
+//global execution context gets deleted once its work is finished
